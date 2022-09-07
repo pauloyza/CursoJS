55d15a450fb9a068f0f92e9cf9b41cda870fa458
pauloyza <andrepaulo@id.uff.br>
2022-09-07 14:09:44 -0300

    Operadores, incrementadores e parse

---
+++ aula10/index.js
@@ -0,0 +1,17 @@
+let num1 = 2
+console.log(num1++)
+console.log(num1)
+
+num1 = 2
+console.log(++num1)
+console.log(num1)
+
+const num2 = 2
+// console.log(num2++) error case
+
+const num3 = '10'
+const num4 = '10.5'
+console.log(parseInt(num3))
+console.log(parseFloat(num4))
+
+// A função Number() tbm existe
