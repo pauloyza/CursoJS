c1980b2135913bfdce8dc068fdfb861a512a464e
pauloyza <andrepaulo@id.uff.br>
2022-09-07 10:11:28 -0300

    diferenca entre let e const

---
+++ aula09/index.js
@@ -0,0 +1,7 @@
+var nome ='Paulo'
+var nome ='Joao'
+
+/**
+ * Principal diferença entre var e let, eh que let te proíbe de recriar novamente uma variável
+ * uma boa prática é nunca criar uma variável sem utilizar ou o let ou o const
+ */
