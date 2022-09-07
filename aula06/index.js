52c3289f400e4d1d7fd75bb77768df0685bb3302
pauloyza <andrepaulo@id.uff.br>
2022-09-07 09:30:39 -0300

    Usando let e boas praticas

---
+++ aula06/index.js
@@ -0,0 +1,15 @@
+/**
+ * Não podemos criar variáveis com palavras reservadas
+ * Variáveis precisam ter nomes significativos
+ * Não pode começar o nome de uma variável com um número
+ * Não podem conter espaços ou traços
+ * Utilizamos camelCase
+ * Case-sensitive
+ * Não podemos redeclarar variáveis com let
+ * NÃO UTILIZE VAR, UTILIZE LET.
+ */
+
+let nomeCliente = "João"
+let nomecliente = "Otavio"
+
+console.log(nomeCliente, nomecliente)
