1772076652ada7fda80ca1206aa619c7c65c3c88
pauloyza <andrepaulo@id.uff.br>
2022-09-09 07:49:29 -0300

    Comandos basicos do window usando tem strings

---
+++ aula11/js/script.js
@@ -0,0 +1,7 @@
+alert('Com a nossa mensagem')
+
+let num1 = prompt("Digite um número:")
+let num2 = prompt("Digite um outro número:")
+let resultado = Number(num1) + parseFloat(num2)
+
+alert(`O resultado da soma desses números é: ${resultado}`)
