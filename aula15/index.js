64ade203662643b4b8d9d90d1ffd2618c4e3ab47
pauloyza <andrepaulo@id.uff.br>
2022-09-09 14:07:48 -0300

    Principios da biblioteca Math

---
+++ aula15/index.js
@@ -0,0 +1,13 @@
+console.log(!!100/0)//cuidado
+
+let num1 = 9.7
+let num2 = Math.floor(num1) // arredonda pra baixo
+num2 = Math.ceil(num1) // arredonda pra cima
+num2 = Math.round(num1) // arredonda
+
+// Math.max() e Math.min() para máximo e minimo
+
+// const aleatorio = Math.random() * (teto - piso) + piso // nunca inclui o teto
+
+
+console.log(num2)
