14722daecd4a39c94d192e965286c1f6a36bddba
pauloyza <andrepaulo@id.uff.br>
2022-09-09 15:07:12 -0300

    exercicios com innerhtml e math

---
+++ aula15/ex/index.js
@@ -0,0 +1,9 @@
+let num1 = prompt("Digite um número:")
+num1 = Number(num1)
+document.body.innerHTML += `<h2><strong>Seu número é ${num1}</strong></h2> <br />`
+document.body.innerHTML += `Raiz quadrada: ${num1 ** 0.5} <br />`
+document.body.innerHTML += `${num1} é inteiro: ${Number.isInteger(num1)} <br />`
+document.body.innerHTML += `É NaN: ${Number.isNaN(num1)} <br />`
+document.body.innerHTML += `Arredondando para baixo: ${Math.floor(num1)} <br />`
+document.body.innerHTML += `Arredondando para cima ${Math.ceil(num1)} <br />`
+document.body.innerHTML += `Com duas casas decimais: ${num1.toFixed(2)} <br />`
