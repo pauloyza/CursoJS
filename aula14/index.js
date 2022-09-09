8fce940b4cfc8f50ffa4e69cf9f2b6942f5c03b1
pauloyza <andrepaulo@id.uff.br>
2022-09-09 13:45:35 -0300

    Exemplos da biblioteca number

---
+++ aula14/index.js
@@ -0,0 +1,17 @@
+/**
+ * Tópicos de operações com number
+ */
+
+num1 = 1998.1232232
+num2 = 2.5
+
+//  console.log(num1.toString() + num2)
+// num1 = num1.toString()
+// console.log(num1.toString(16)) // o argumento do método pode transformar em binario ou hexa
+// console.log(num1.toFixed(2)) // controle de casa decimais
+// console.log(Number.isInteger(num1))
+// console.log(Number.isNaN(num1)) 
+
+
+
+
