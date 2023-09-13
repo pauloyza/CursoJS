c984182499455b2f5d14bc0c7c74a7883bf77fc4
pauloyza <andrepaulo@id.uff.br>
2023-09-13 14:03:52 -0300

    termino da aula 23 sobre operadores ternarios

--- aula23/index.js
+++ aula23/index.js
@@ -1,3 +1,6 @@
 const pontuacaoUsuario = 999
 const nivelUsuario = pontuacaoUsuario >= 1000? "Vip" : "Comum"
-console.log(nivelUsuario)
+
+const corUsuario = null
+const corPadrao = corUsuario || 'Preta'
+console.log(nivelUsuario,corPadrao)
