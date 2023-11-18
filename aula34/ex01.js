191bc984ca74aeb48cbefec7d2b7da0d6b5d9267
pauloyza <andrepaulo@id.uff.br>
2023-11-18 20:47:09 -0300

    exer logica

---
+++ aula34/ex01.js
@@ -0,0 +1,19 @@
+function maior(num1,num2)
+{
+    return num1>num2? num1 : num2;
+}
+
+const max2 = (x,y) => x>y ? x:y;
+
+const x = 1080
+const y = 1920  
+
+console.log(maior(x,y))
+console.log(max2(x,y))
+
+//////////////
+
+const isPaisagem = (l,h) => l>h;
+console.log(isPaisagem(x,y))
+
+//////
